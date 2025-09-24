ab7c08a70a5b10ee1b8ade1613c19b6fe4a8428b
HTK-nyai <hottokenyai@gmail.com>
2025-09-24 11:28:41 +0900

    Update Chip.java

--- Chip.java
+++ Chip.java
@@ -1,28 +1,30 @@
 class Chip{
     public static final int INITIAL_CHIP_COUNT = 50;
-    public static final int CHIP_FLUCTUATION = 10;
-    public static final int CHIP_FLUCTUATION_BJ = 15;
+    public static final int CHIP_FLUCTUATION_Base = 10;
+    public static final double CHIP_FLUCTUATION_adj_BJ = 1.5;
+    public static final double CHIP_FLUCTUATION_adj_DD = 2;
     
-	int chipCount=0;	
-
+	int chipCount=0;
+	
 	Chip(){
 		chipCount = INITIAL_CHIP_COUNT;
 	}
 	
-	void addChip(boolean BJflag) { //addとremoveはまとめてもいいかも
-		if(BJflag) {
-			chipCount+=CHIP_FLUCTUATION_BJ;
-		}else {
-			chipCount+=CHIP_FLUCTUATION;
-		}
+	int calculateDelta(boolean isBJ, boolean isDD) {
+		double multiplier = 1.0;
+		if (isBJ) multiplier *= CHIP_FLUCTUATION_adj_BJ;
+		if (isDD) multiplier *= CHIP_FLUCTUATION_adj_DD;
+		return (int) Math.round(CHIP_FLUCTUATION_Base * multiplier);
 	}
 	
-	void removeChip(boolean BJflag) {
-		if(BJflag) {
-			chipCount-=CHIP_FLUCTUATION_BJ;
-		}else {
-			chipCount-=CHIP_FLUCTUATION;
-		}
+	void addChip(boolean isBJ, boolean isDD) { //addとremoveはまとめてもいいかも
+		int delta = calculateDelta(isBJ, isDD);
+		chipCount += delta;	
+	}
+	
+	void removeChip(boolean isBJ, boolean isDD) { //addとremoveはまとめてもいいかも
+		int delta = calculateDelta(isBJ, isDD);
+		chipCount -= delta;	
 	}
 	
 	void showChipCount() {
