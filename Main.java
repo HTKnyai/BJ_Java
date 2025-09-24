7af7bff96117e3385b6f307a131023e23f6f5de9
HTK-nyai <hottokenyai@gmail.com>
2025-09-24 09:38:11 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,39 +2,46 @@ public class Main {
 
 	public static void main(String[] args) {
         
-		boolean continueFlag = true;
+		boolean gameContinueFlag = true;
+		boolean roundContinueFlag = true;
         GameManager gameManager = new GameManager();
 
 		UI ui = new UI();
-        Chip chip = new Chip();
-        
-    	// === ゲーム開始メッセージ ===
-		ui.showTitle();
-        ui.showStartMessage();
-        
-        while(continueFlag) {		
-            // === 山札と手札の準備 ===
+
+        while(gameContinueFlag) {
+	    	// === ゲーム開始メッセージ ===
+			ui.showTitle();
+	        ui.showStartMessage();
+
+	        Chip chip = new Chip();
             Deck deck = new Deck();
             deck.shuffle();
-            
-	        Hand playerHand = new Hand();
-	        Hand dealerHand = new Hand();
-	        gameManager.setUpHands(deck, playerHand, dealerHand);
 	        
-	        // === 初期手札の表示 ===
-	        ui.showDealerHandLimited(dealerHand);	        		    
-	        ui.showPlayerHand(playerHand);
+	        do{		
+	            // === 手札の準備 ===           y
+				ui.showLoading();
+		        Hand playerHand = new Hand();
+		        Hand dealerHand = new Hand();
+		        gameManager.setUpHands(deck, playerHand, dealerHand);
+		        
+		        // === 初期手札の表示 ===
+		        ui.showDealerHandLimited(dealerHand);	        		    
+		        ui.showPlayerHand(playerHand);
+		        
+		        // === プレイヤー/ディーラーのターン ===
+		    	ui.showLoading();
+		        gameManager.playerTurn(deck, playerHand);
+		        gameManager.dealerTurn(dealerHand, deck);
+		
+		        // === 勝敗判定・状態更新 ===
+		        gameManager.applyResult(playerHand,dealerHand,chip);
+		        
+		        // === 続行処理 ===
+		        roundContinueFlag = gameManager.askNextRound(chip);
+	        }while(roundContinueFlag) ;
 	        
-	        // === プレイヤー/ディーラーのターン ===
-	        gameManager.playerTurn(deck, playerHand);
-	        gameManager.dealerTurn(dealerHand, deck);
-	
-	        // === 勝敗判定・状態更新 ===
-	        gameManager.applyResult(playerHand,dealerHand,chip);
-	        
-	        // === 続行処理 ===
-	        continueFlag = gameManager.hasChipsRemain(chip) && gameManager.askNextRound();
+	    	ui.endGame(chip);
+	    	gameContinueFlag = gameManager.askNextGame();
         }
-    	ui.print("ゲームを終了します!");
 	}
 }
