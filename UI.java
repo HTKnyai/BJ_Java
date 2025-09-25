32522cf583e7c1fefcae6be2d96b6e5dbcbbad4f
HTK-nyai <hottokenyai@gmail.com>
2025-09-25 12:55:05 +0900

    Update UI.java

--- UI.java
+++ UI.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -42,6 +43,11 @@ public class UI {
 
 	}
 	
+	void refillDeck(Deck deck) {
+		print("山札を補充　残り："+deck.cardsInDeck.size()+"枚");
+	}
+	
+	/* 文字形式のトランプ表示
     void showHand(Hand hand,String member) {
         print("=="+member+"の手札==");
         for(Card eachcards:hand.cardsInHand) {
@@ -51,14 +57,52 @@ public class UI {
         print("====================");
         print();
     };
+    */
+    
+    void printCards(ArrayList<Card> cards, boolean hidden) {
+        StringBuilder line1 = new StringBuilder();
+        StringBuilder line2 = new StringBuilder();
+        StringBuilder line3 = new StringBuilder();
+        StringBuilder line4 = new StringBuilder();
+
+        for(int i=0;i<cards.size();i++) {
+        	
+        	if(i==1 && hidden==true) {
+	            line1.append("┌─┐");
+	            line2.append("│? │");
+	            line3.append("│ ?│");
+	            line4.append("└─┘");	
+        	}else {      	
+	        	String rank = cards.get(i).rank;
+	        	String suit = cards.get(i).suit;
+	        	
+	        	String rank_fixed=rank+(rank.length()==2?"":" ");
+	        	
+	            line1.append("┌─┐");
+	            line2.append("│").append(rank_fixed).append("│");
+	            line3.append("│ ").append(suit).append("│");
+	            line4.append("└─┘");	
+        	}
+        }
+        
+        print(line1.toString());
+        print(line2.toString());
+        print(line3.toString());
+        print(line4.toString());
+    }
 	
 	void showPlayerHand(Hand hand) {
-        showHand(hand,"プレイヤー");
+	    print("==プレイヤーの手札==");
+        printCards(hand.cardsInHand,false);
+        print("====================");
         print();
-}
+	}
+	
 	
 	void showDealerHand(Hand hand) {
-        showHand(hand,"ディーラー");
+	    print("==ディーラーの手札==");
+        printCards(hand.cardsInHand,false);
+        print("====================");
         if(hand.isBust()) {
     		dealerTalk("dealerBust");
         }
@@ -67,22 +111,16 @@ public class UI {
 	
 	void showDealerHandLimited(Hand hand) {
 	    print("==ディーラーの手札==");
-    	//System.out.print("   ");
-	    hand.cardsInHand.get(0).printCard();
-	    for(int i=1;i<hand.cardsInHand.size();i++) {
-        	//System.out.print("   ");
-	    	print("suit:?,rank:?");
-	    }
+        printCards(hand.cardsInHand,true);
         print("====================");
 	    print();
 	}
 	
-    void showTurnResult(Hand playerHand,Hand dealerHand,String result,Chip chip) {
+    void showTurnResult(Hand playerHand,Hand dealerHand,String result,Chip chip, int chipDelta) {
     	int playerScore=playerHand.countScore();
     	int dealerScore=dealerHand.countScore();
     	
         showDealerHand(dealerHand);
-        
 
        	print("ディーラー:"+dealerScore);
        	print("プレイヤー:"+playerScore);
@@ -90,9 +128,33 @@ public class UI {
     	if(dealerHand.isBust())showBust("ディーラー");
         print();
         print("結果："+result);
-        print("チップ："+chip.chipCount);
+        
+        String sign = result.equals("DEALER_WINS") ? "-":"+";        
+        print("チップ変動: " + sign + chipDelta);
+        print("現在のチップ："+chip.chipCount);
         showLoading(300);
         print();
+    }
+    
+    int askBet(Scanner scanner, Chip chip) {
+    	print("賭けるチップの枚数を入力してください（10～"+chip.chipCount+"枚）");
+    	int betAmount=0;
+    	
+        while(true) {
+        	try {
+        		betAmount = Integer.parseInt(scanner.nextLine());
+        		if(10<=betAmount && betAmount<=chip.chipCount) {
+        			break;
+        		}else {
+        			print("10枚以上"+chip.chipCount+"枚以下の整数を入力してください");
+        		}
+        	}catch(NumberFormatException e) {
+        		print("正しい数値を入力してください。");
+        	}      	
+	        
+        }
+        
+        return betAmount;
     }
 	
 	boolean askYN(Scanner scanner,String message) {
@@ -128,6 +190,17 @@ public class UI {
 	    );
 	}
 	
+	void showBJ() {
+		print(
+			"██████╗      ██╗██╗██╗\n"+
+			"██╔══██╗     ██║██║██║\n"+
+			"██████╔╝     ██║██║██║\n"+
+			"██╔══██╗██   ██║╚═╝╚═╝\n"+
+			"██████╔╝╚█████╔╝██╗██╗\n"+
+			"╚═════╝  ╚════╝ ╚═╝╚═╝\n"                     
+		);
+	}
+	
 	void showLoading() {
 	    showLoading(500);
 	}
@@ -145,6 +218,18 @@ public class UI {
 		print();
 	}
 	
+	void showOver1000() {
+		print(
+			" █████╗ ███╗   ███╗ █████╗ ███████╗██╗███╗   ██╗ ██████╗ ██╗██╗\n"+
+			"██╔══██╗████╗ ████║██╔══██╗╚══███╔╝██║████╗  ██║██╔════╝ ██║██║\n"+
+			"███████║██╔████╔██║███████║  ███╔╝ ██║██╔██╗ ██║██║  ███╗██║██║\n"+
+			"██╔══██║██║╚██╔╝██║██╔══██║ ███╔╝  ██║██║╚██╗██║██║   ██║╚═╝╚═╝\n"+
+			"██║  ██║██║ ╚═╝ ██║██║  ██║███████╗██║██║ ╚████║╚██████╔╝██╗██╗\n"+
+			"╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝\n"	
+		);
+		
+	}
+	
 	Random rand = new Random();
 	
 	void dealerTalk(String Situation) {
@@ -159,12 +244,17 @@ public class UI {
 	                "落ち着いて参りましょう"
 	            };
 	            break;
+	        case "naturalBJ":
+	            lines = new String[] {
+	                "おや... これはこれは",
+	            };
+	            break;
 	        case "playerTurn":
 	            lines = new String[] {
 	                "どうされますかな...",
-	                "ヒット？それともステイか...",
-	                "ほう……",
-	                "慎重に参りましょうか…",
+	                "ほほう...",
+	                "ほう......",
+	                "慎重に参りましょうか...",
 	                "焦らずに判断されよ"
 	            };
 	            break;
@@ -179,9 +269,9 @@ public class UI {
 	            break;
 	        case "dealerTurn":
 	            lines = new String[] {
-	                "ふむ…",
-	                "では…",
-	                "私も参りますぞ…",
+	                "ふむ...",
+	                "では...",
+	                "私も参りますぞ...",
 	                "少々手加減はせぬといけませんな",
 	                "じっくり行きますぞ"
 	            };
@@ -190,26 +280,26 @@ public class UI {
 	            lines = new String[] {
 	                "うむ、見事な腕前。感心しましたな",
 	                "お見事、まだまだ油断は禁物ですぞ",
-	                "フフフ…見事な勝利ですな",
+	                "フフフ...見事な勝利ですな",
 	                "よくやられましたな",
 	                "勘が冴えておりますな",
 	            };
 	            break;
 	        case "dealerWin":
 	            lines = new String[] {
-	                "ほほう……運が味方しておるようです",
-	                "ほほほ、運も実力のうち……",
+	                "ほほう......運が味方しておるようです",
+	                "ほほほ、運も実力のうち......",
 	                "勝負とは最後までわからぬものですな",
-	                "フフフ…残念でしたな",
+	                "フフフ...残念でしたな",
 	                "次に期待しましょう",
 	            };
 	            break;
 	        case "draw":
 	            lines = new String[] {
-	                "まだまだ楽しめそうですな、フフフ……",
+	                "まだまだ楽しめそうですな、フフフ......",
 	                "なかなか拮抗しておりますな",
 	                "フフフ、良い勝負でしたぞ",
-	                "油断はできませんな…",
+	                "油断はできませんな...",
 	                "これぞ運と実力のせめぎ合いですな"
 	            };
 	            break;
@@ -225,28 +315,32 @@ public class UI {
 	            break;
 	        case "dealerBust":
 	            lines = new String[] {
-	                "おおっと…これはこれは……",
-	                "少々熱が入りすぎましたかな…？",
+	                "おおっと...これはこれは......",
+	                "少々熱が入りすぎましたかな...？",
 	                "うむ、やはり油断は禁物ですな",
 	            };
 	            break;
 	        case "endGame":
 	            lines = new String[] {
-	                "また、お待ちしておりますぞ…",
-	                "それではまた…",
+	                "また、お待ちしておりますぞ...",
+	                "それではまた...",
 	                "楽しませていただきました",
 	                "またのお越しを",
-	                "ゆっくり休んでくださいませ"
+	                "ゆっくりお休みくださいませ"
 	            };
 	            break;
 	        case "endOver100":
 	        	lines = new String[] {
-	        		"これはこれは... 見事というほかありませんな",
+	        		"御見逸れしました",
+	        		"お見事!",
+	        	};
+	        	break;
+	        case "endOver1000":
+	        	lines = new String[] {
+	        		"これはこれは...",
 	        		"あっぱれにございます",
 	        		"...素晴らしいものをお持ちのようですな"
 	        	};
-	        	break;
-
 	    }
 		print("「"+lines[rand.nextInt(lines.length)]+"」");
 		print("");
@@ -254,7 +348,12 @@ public class UI {
 	
 	void endGame(Chip chip) {
 		print("今回のゲームを終了します!");
-		if(chip.chipCount>=100) {
+		if(chip.chipCount>=1000) {
+			print();
+			showOver1000();
+			dealerTalk("endOver1000");
+		}
+		else if(chip.chipCount>=100) {
 			dealerTalk("endOver100");
 		}
     	dealerTalk("endGame");
